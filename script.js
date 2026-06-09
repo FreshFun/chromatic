2ba13605d646b738a93276557f5ebfc8eb766007
FreshFun <freshfunnyfresh@gmail.com>
2026-06-08 21:24:06 -0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('playBtn').addEventListener('click', () => {
-  // start game
-});
-
-document.getElementById('settingsBtn').addEventListener('click', () => {
-  // open settings
-});
